38e902a58ddda852961af3cd1504152c2bb2eced
k3vinfoo <kf1270@nyu.edu>
2018-03-21 00:44:27 -0400

    new site changes

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#nyc').click(function() {
+        console.log('nyc');
+        $('#contentPage').css('background-image', 'url("' + 'https://www.flightclub.com/media/catalog/product/6/3/63611742962-air-jordan-1-retro-high-og-bred-black-varsity-red-white-011845_2.jpg' + '")');
+    });
+    $('#mediaArts').click(function() {
+        console.log('mediaArts');
+        $('#contentPage').css('background-image', 'url("' + 'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/a/i/air-jordan-2-retro-infrared-23-black-infrared-23-pr-pltnm-wht-011906_1.png' + '")');
+    });
+    $('#creative').click(function() {
+        console.log('creative');
+
+        $('#contentPage').css('background-image', 'url("' + 'https://stockx.imgix.net/Nike-Air-Air-Jordan-3-JTH-Update.png?fit=fill&bg=FFFFFF&w=1400&h=1000&auto=format,compress&trim=color&q=40' + '")');
+    });
+    $('#design_text').click(function() {
+        console.log('design');
+        $('#contentPage').css('background-image', 'url("' + 'https://images.solecollector.com/complex/image/upload/c_fill,f_auto,fl_lossy,q_auto,w_1100/air-jordan-4-retro-og-hero_frnqkt.jpg' + '")');
+    });
+    $('#thinking_text').click(function() {
+        console.log('thinking');
+        $('#contentPage').css('background-image', 'url("' + 'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/a/i/air-jordan-5-retro-gg-gs-hot-lava-wolf-grey-black-hot-lava-white-012231_1.jpg' + '")');
+    });
+    $('#making_text').click(function() {
+        console.log('making');
+        $('#contentPage').css('background-image', 'url("' + 'https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/a/i/air-jordan-6-retro-low-infrared-white-infrared-23-black-012255_1.jpg' + '")');
+    });
+    $('#cool_text').click(function() {
+        console.log('cool_text');
+        $('#contentPage').css('background-image', 'url("' + 'https://www.flightclub.com/media/catalog/product/0/1/011617_1_1.jpg' + '")');
+    });
+});
